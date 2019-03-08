771bbde8ead673816ccd9761acb786c60a365de1
giorgioT <tonzanu@tiscali.it>
2019-03-08 07:35:13 +0100

    Created new project

---
+++ projects/Analisi_progettazione_sw/MoveFiles/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoveFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

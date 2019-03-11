15a780a3a2524f505cff3fa4fbbd339cff2f5472
LuigiFoddai <luigi.foddai@live.it>
2019-03-11 18:36:09 +0100

    Signed-off-by: LuigiFoddai <luigi.foddai@live.it>

--- projects/Analisi_progettazione_sw/MoveFiles/Program.cs
+++ projects/Analisi_progettazione_sw/MoveFiles/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace MoveFiles
 {
@@ -7,6 +8,26 @@ namespace MoveFiles
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string sourceDirectory = @"c:\file_uploaded";
+            DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+            foreach (DirectoryInfo diSourceSubDir in diSource.GetDirectories())
+            {
+                Console.WriteLine(diSourceSubDir.Name);
+                foreach (var fileInfo in diSourceSubDir.GetFiles())
+                {
+                    Console.WriteLine(fileInfo.Name);
+                    if (fileInfo.Name.StartsWith("CV"))
+                    {
+                        Console.WriteLine("Questo è un curriculum");
+                    }
+                    else if (fileInfo.Name.StartsWith ("CERT"))
+                    {
+                        Console.WriteLine("Questa è una certificazione");
+                    }
+                    
+                }
+               
+            }
         }
     }
 }

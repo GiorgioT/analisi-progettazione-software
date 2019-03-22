de5c598f5e5872ead05699a267bd05352e8058e9
Daniele Lurani <dannylu@tiscali.it>
2019-03-22 15:54:33 +0100

    roba mia

---
+++ projects/Analisi_progettazione_sw/Bancomat_DanieleLurani/MoveFiles/CopyAll.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MoveFiles
+{
+    public class CopyAll
+    {
+        public static void MoveFiles(DirectoryInfo source, DirectoryInfo target)
+        {
+            Console.WriteLine("Hello World!");
+            string sourceDirectory = @"D:\VSProjects\upload";
+            string CVdestinationDirectory = @"D:\TEMPORANEA_APPOGGIO\Tex\uploads\Curricula\";
+            string CERTdestinationDirectory = @"D:\TEMPORANEA_APPOGGIO\Tex\uploads\Certificazioni\";
+            var destination = string.Empty;
+            DirectoryInfo diSource = new DirectoryInfo(sourceDirectory);
+            foreach (DirectoryInfo diSourceSubDir in diSource.GetDirectories())
+            {
+                Console.WriteLine(diSourceSubDir.Name);
+                foreach (var filedentrolacartella in diSourceSubDir.GetFiles())
+                {
+                    Console.WriteLine(filedentrolacartella.Name);
+                    if (filedentrolacartella.Name.StartsWith("CV"))
+                    {
+                        Console.WriteLine("Questo è un curriculum");
+                        destination = CVdestinationDirectory + filedentrolacartella.Name;
+                    }
+                    else if (filedentrolacartella.Name.StartsWith("CERT"))
+                    {
+                        Console.WriteLine("Questa è una certificazione");
+                        destination = CERTdestinationDirectory + filedentrolacartella.Name;
+                    }
+                    filedentrolacartella.MoveTo(destination);
+
+                }
+            }
+        }
+    }
+}
---
+++ projects/Analisi_progettazione_sw/Bancomat_DanieleLurani/MoveFiles/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace MoveFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Inserisci la tua password");
+                var input = Console.ReadLine();
+                var checkP = Bancomat.CheckPassword(input);
+                if (!checkP)
+                {
+                    Console.WriteLine("La tua password non è stata confermata");
+                    Console.WriteLine("Riprova ad inserire la password");
+                }
+                else
+                {
+                    Console.WriteLine("password verificata");
+                    break;
+                }
+            }
+            Console.WriteLine("Scegli operazione");
+            Console.WriteLine("Scegli 1 per deposito");
+            Console.WriteLine("Scegli 2 per prelievo");
+            var operation =  Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hai scelto di fare un " + 
+                Enum.GetName(typeof(BacomatEnums.OperationType), operation));
+            Console.WriteLine("Inserisci l'importo");
+            var amount = Convert.ToDecimal(Console.ReadLine());
+
+            switch (operation)
+            {
+                case (int)BacomatEnums.OperationType.Deposito:
+                    break;
+                case (int)BacomatEnums.OperationType.Prelievo:
+                    var checkA = Bancomat.CheckAmount(amount);
+                    while (!checkA)
+                    {
+                        Console.WriteLine("Importo richiesto non disponibile");
+                        Console.WriteLine("Inserisci l'importo");
+                        amount = Convert.ToDecimal(Console.ReadLine());
+                        checkA = Bancomat.CheckAmount(amount);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+
+        }
+    }
+}
+
+
+
+

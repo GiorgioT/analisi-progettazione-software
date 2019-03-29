cbf43426027887044feb9064d8b33d4921ed8146
DanieleL96 <dannylu@tiscali.it>
2019-03-29 13:42:17 +0100

    lezione 29.03

---
+++ projects/Analisi_progettazione_sw/Bancomat_DanieleLurani/Ereditarietà/vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ereditarietà
+{
+    class vehicle
+    {
+        protected int _maxSpeed, currentSpeed;
+        private int numberOfPassengers, yearBuilt;
+        public int MaxSpeed { get; set; }
+        public string ModelName { get; set; }
+        public void accellerate(int time)
+        {
+           var a = currentSpeed / time;
+        }
+        public void brake(int time)
+        {
+            var b = -1 * (currentSpeed / time);
+        }
+        public void printInfo()
+        {
+            Console.WriteLine("Modello: {0}, Velocità {1}", ModelName, currentSpeed);
+            Console.WriteLine($"Modello: {ModelName}, Velocità {currentSpeed}");
+        }
+
+    }
+}

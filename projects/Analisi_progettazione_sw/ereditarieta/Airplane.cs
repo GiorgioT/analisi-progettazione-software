25311234ceadf58db617538e2a582b1a767cb40c
giorgioT <tonzanu@tiscali.it>
2019-04-11 17:42:55 +0200

    project update
    Signed-off-by: giorgioT <tonzanu@tiscali.it>

---
+++ projects/Analisi_progettazione_sw/ereditarieta/Airplane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ereditarieta
+{
+    public class Airplane: FlyingVehicle
+    {
+        public Airplane()
+        {
+            MaxSpeed = 1000;
+        }
+
+        public override void TakeOffPianoPiano()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ projects/Analisi_progettazione_sw/ereditarieta/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ereditarieta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fiatPunto = new StreetVehicle(4);
+            fiatPunto.MaxSpeed = 90;
+            fiatPunto.ModelName = "Punto";
+
+            fiatPunto.PrintInfo();
+
+            var cesna = new Airplane();
+            cesna.ModelName = "Cesna 10447";
+            cesna.TakeOff();
+            cesna.PrintInfo();
+            
+
+            var veic = new Vehicle();
+
+
+            Vehicle v = new Airplane();
+            
+        }
+    }
+}

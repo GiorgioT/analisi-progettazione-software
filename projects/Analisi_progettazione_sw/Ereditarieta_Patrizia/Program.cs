8a11031850c3a9f0430ad036a6b64d8d93276e2e
PatriziaSas <patry.ps2@gmail.com>
2019-04-12 10:49:10 +0200

    Signed-off-by: PatriziaSas <patry.ps2@gmail.com>

---
+++ projects/Analisi_progettazione_sw/Ereditarieta_Patrizia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ereditarieta_Patrizia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var veic = new Vehicle_Patrizia();
+            
+            var fv = new FlyingVehicle_Patrizia();
+            fv.MaxSpeed = 1000;
+            fv.ModelName = "Airbus 259";
+            fv.TakeOff();
+            fv.PrintInfo();
+            Console.WriteLine("We are flying at 8000 meters");
+            fv.Land();
+        }
+    }
+}
